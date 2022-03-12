7b3a4240597534c1031c5bcfe93a664f88c40618
Nyior <nyior.clement@aun.edu.ng>
2022-03-12 10:44:45 +0100

    use arrow function

--- utils.js
+++ utils.js
@@ -1,8 +1,10 @@
-function complete(){
-    const allBars = document.querySelectorAll('.bar')
+const complete = () => {
+    const allBars = document.querySelectorAll('.bar');
     allBars.forEach(anchor => {
         anchor.setAttribute('style', 'width: 100%') ;
         anchor.innerHTML = '100%';
     });
 }
+
+document.getElementById("btn").addEventListener("click", complete);
 
